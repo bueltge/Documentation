02c47b9ff8e133c27f2957dc38a73c00d2d531fc
Frank Bültge <frank@bueltge.de>
2012-09-05 16:24:02 +0200

    Update for namesapce in defined string for more felexibility

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -2,33 +2,40 @@
 /**
  * Documentation Theme Options
  *
- * @package WordPress
+ * @package    WordPress
  * @subpackage Documentation
- * @since 08/09/2012
+ * @since      08/09/2012
  */
 
 class Documentation_Options {
+	
+	/**
+	 * Identifier, namespace
+	 */
+	var $theme_key = '';
+	
 	/**
 	 * The option value in the database will be based on get_stylesheet()
 	 * so child themes don't share the parent theme's option value.
 	 */
-	var $option_key = 'documentation_theme_options';
+	var $option_key = '';
 	
 	/**
 	 * Initialize our options.
-	 */
+	 */	
 	var $options = array();
 	
-	function documentation_Options() {
+	function __construct() {
+		
 		// Set option key based on get_stylesheet()
-		if ( 'documentation' != get_stylesheet() )
-			$this->option_key = get_stylesheet() . '_theme_options';
-
+		$this->theme_key  = get_stylesheet();
+		$this->option_key = $this->theme_key . '_theme_options';
+		
 		add_action( 'admin_init',         array( $this, 'options_init'       ) );
 		add_action( 'admin_menu',         array( $this, 'add_page'           ) );
 		add_action( 'customize_register', array( $this, 'customize_register' ) );
 	}
-	
+
 	/**
 	 * Register the form setting for our options array.
 	 *
@@ -39,16 +46,17 @@ class Documentation_Options {
 	 * formatted, and safe.
 	 */
 	function options_init() {
+		
 		// Load our options for use in any method.
 		$this->options = $this->get_theme_options();
-
+		
 		// Register our option group.
 		register_setting(
-			'documentation_options',   // Options group, see settings_fields() call in render_page()
-			$this->option_key,         // Database option, see get_theme_options()
-			array( $this, 'validate' ) // The sanitization callback, see validate()
+			$this->theme_key . '_options',    // Options group, see settings_fields() call in render_page()
+			$this->option_key,          // Database option, see get_theme_options()
+			array( $this, 'validate' )  // The sanitization callback, see validate()
 		);
-
+		
 		// Register our settings field group.
 		add_settings_section(
 			'general',        // Unique identifier for the settings section
@@ -56,17 +64,26 @@ class Documentation_Options {
 			'__return_FALSE', // Section callback (we don't want anything)
 			'theme_options'   // Menu slug, used to uniquely identify the page; see add_page()
 		);
-
+		
 		// Register our individual settings fields.
 		add_settings_field(
-			'rewrite_url',                                // Unique identifier for the field for this section
+			'rewrite_url',                                 // Unique identifier for the field for this section
 			__( 'Rewrite URL', 'documentation' ),          // Setting field label
-			array( $this, 'settings_field_rewrite_url' ), // Function that renders the settings field
+			array( $this, 'settings_field_enable_fonts' ), // Function that renders the settings field
+			'theme_options',                               // Menu slug, used to uniquely identify the page; see add_page()
+			'general'                                      // Settings section. Same as the first argument in the add_settings_section() above
+		);
+		
+		// Register our individual settings fields for color
+		add_settings_field(
+			'text_color',                                 // Unique identifier for the field for this section
+			__( 'Text color', 'documentation' ),          // Setting field label
+			array( $this, 'settings_field_text_color' ), // Function that renders the settings field
 			'theme_options',                               // Menu slug, used to uniquely identify the page; see add_page()
 			'general'                                      // Settings section. Same as the first argument in the add_settings_section() above
 		);
 	}
-
+	
 	/**
 	 * Add our theme options page to the admin menu.
 	 *
@@ -77,49 +94,72 @@ class Documentation_Options {
 		$theme_page = add_theme_page(
 			__( 'Theme Options', 'documentation' ), // Name of page
 			__( 'Theme Options', 'documentation' ), // Label in menu
-			'edit_theme_options',                   // Capability required
-			'theme_options',                        // Menu slug, used to uniquely identify the page
-			array( $this, 'render_page' )           // Function that renders the options page
+			'edit_theme_options',                  // Capability required
+			'theme_options',                       // Menu slug, used to uniquely identify the page
+			array( $this, 'render_page' )          // Function that renders the options page
 		);
 	}
 	
 	/**
 	 * Returns the default options.
+	 * Use the hook 'documentation_default_theme_options' for change via plugin
 	 * 
-	 * @return  Array
+	 * @return   Array
 	 */
 	function get_default_theme_options() {
 		
 		$default_theme_options = array(
 			'rewrite_url' => 'wp-admin/edit.php',
+			'text_color'  => '#333'
 		);
 		
-		return apply_filters( 'documentation_default_theme_options', $default_theme_options );
+		return apply_filters( $this->theme_key . '_default_theme_options', $default_theme_options );
 	}
 	
 	/**
 	 * Returns the options array.
 	 * 
-	 * @return  String
+	 * @return   Array
 	 */
 	function get_theme_options() {
 		
-		return get_option( $this->option_key, $this->get_default_theme_options() );
+		$saved = (array) get_option( $this->option_key );
+		$defaults = $this->get_default_theme_options();
+		
+		$options = wp_parse_args( $saved, $defaults );
+		$options = array_intersect_key( $options, $defaults );
+		
+		return $options;
 	}
-
+	
 	/**
-	 * Renders the input field
-	 * 
-	 * @return  void
+	 * Renders the enable fonts checkbox setting field.
 	 */
-	function settings_field_rewrite_url() {
+	function settings_field_enable_fonts() {
 		
 		$options = $this->options; ?>
 		<label for="enable-fonts"> 
-			<input type="text" name="<?php echo $this->option_key; ?>[rewrite_url]" id="rewrite-url" value="<?php echo $options['rewrite_url']; ?>" class="regular-text code" />
+			<input type="text" name="<?php echo $options; ?>[rewrite_url]" id="rewrite-url" value="<?php echo $options['rewrite_url']; ?>" class="regular-text code" />
 			<br /><span class="description"><?php printf( __( 'Edit an URL in Backend for the Administration Link in Frontend. Example: %s', 'documentation' ), '<code>wp-admin/edit.php</code>' ); ?> </span>
 		</label>
-	<?php }
+	<?php
+	}
+	
+	/**
+	 * Render text field for text color
+	 * 
+	 * @since    09/05/2012
+	 * @return   void
+	 */
+	public function settings_field_text_color() {
+		
+		$options = $this->options; ?>
+		<label for="text-color"> 
+			<input type="text" name="<?php echo $options; ?>[text_color]" id="text-color" value="<?php echo $options['text_color']; ?>" class="regular-text code" />
+			<br /><span class="description"><?php printf( __( 'Fill with an hex code for the text color. Example: %s', 'documentation' ), '<code>#f0f0f0</code>' ); ?> </span>
+		</label>
+	<?php
+	}
 	
 	/**
 	 * Returns the options array.
@@ -136,7 +176,7 @@ class Documentation_Options {
 
 			<form method="post" action="options.php">
 				<?php
-					settings_fields( 'documentation_options' );
+					settings_fields( $this->theme_key . '_options' );
 					do_settings_sections( 'theme_options' );
 					submit_button();
 				?>
@@ -144,7 +184,7 @@ class Documentation_Options {
 		</div>
 		<?php
 	}
-
+	
 	/**
 	 * Sanitize and validate form input. Accepts an array, return a sanitized array.
 	 *
@@ -155,19 +195,20 @@ class Documentation_Options {
 		$output = $defaults = $this->get_default_theme_options();
 
 		// The enable fonts checkbox should boolean TRUE or FALSE
-		if ( ! isset( $input['rewrite_url'] ) )
-			$input['rewrite_url'] = FALSE;
+		if ( isset( $input['rewrite_url'] ) && ! empty( $input['rewrite_url'] ) )
+			$output['rewrite_url'] = wp_filter_nohtml_kses( $input['rewrite_url'] );
 		
-		$output['rewrite_url'] = trim( esc_attr( $input['rewrite_url'] ) );
+		if ( isset( $input['text_color'] ) && ! empty( $input['text_color'] ) )
+			$output['text_color'] = wp_filter_nohtml_kses( $input['text_color'] );
 		
-		return apply_filters( 'documentation_options_validate', $output, $input, $defaults );
+		return apply_filters( $this->theme_key . '_options_validate', $output, $input, $defaults );
 	}
-
+	
 	/**
 	 * Implement theme options into Theme Customizer
 	 *
-	 * @param   $wp_customize Theme Customizer object
-	 * @return  void
+	 * @param $wp_customize Theme Customizer object
+	 * @return void
 	 *
 	 * @since 08/09/2012
 	 */
