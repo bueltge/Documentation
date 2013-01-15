ff39500372dab08b6c9123e8e263525e99b2d775
Frank Bültge <frank@bueltge.de>
2013-01-15 11:35:35 +0100

    update responsive table to work always

--- js/responsive-tables.dev.js
+++ js/responsive-tables.dev.js
@@ -1,27 +1,27 @@
 jQuery(document).ready(function($) {
-  var switched = false;
-  var updateTables = function() {
-    if (($(window).width() < 767) && !switched ){
-      switched = true;
-      $("table.responsive").each(function(i, element) {
-        splitTable($(element));
-      });
-      return true;
-    }
-    else if (switched && ($(window).width() > 767)) {
-      switched = false;
-      $("table.responsive").each(function(i, element) {
-        unsplitTable($(element));
-      });
-    }
-  };
-   
-  $(window).load(updateTables);
-  $(window).bind("resize", updateTables);
-   
 	
-	function splitTable(original)
-	{
+	var switched = false;
+	var updateTables = function() {
+		if (($(window).width() < 767) && !switched ){
+			switched = true;
+			$("#content table").each(function(i, element) {
+				splitTable($(element));
+			});
+			return true;
+		}
+		else if (switched && ($(window).width() > 767)) {
+			switched = false;
+			$("#content table").each(function(i, element) {
+				unsplitTable($(element));
+			});
+		}
+	};
+	 
+	$(window).load(updateTables);
+	$(window).bind("resize", updateTables);
+	
+	function splitTable(original) {
+		
 		original.wrap("<div class='table-wrapper' />");
 		
 		var copy = original.clone();
@@ -32,35 +32,37 @@ jQuery(document).ready(function($) {
 		copy.wrap("<div class='pinned' />");
 		original.wrap("<div class='scrollable' />");
 
-    setCellHeights(original, copy);
+		setCellHeights(original, copy);
 	}
 	
 	function unsplitTable(original) {
-    original.closest(".table-wrapper").find(".pinned").remove();
-    original.unwrap();
-    original.unwrap();
+		
+		original.closest(".table-wrapper").find(".pinned").remove();
+		original.unwrap();
+		original.unwrap();
 	}
 
-  function setCellHeights(original, copy) {
-    var tr = original.find('tr'),
-        tr_copy = copy.find('tr'),
-        heights = [];
+	function setCellHeights(original, copy) {
+		
+		var tr = original.find('tr'),
+				tr_copy = copy.find('tr'),
+				heights = [];
 
-    tr.each(function (index) {
-      var self = $(this),
-          tx = self.find('th, td');
+		tr.each(function (index) {
+			var self = $(this),
+					tx = self.find('th, td');
 
-      tx.each(function () {
-        var height = $(this).outerHeight(true);
-        heights[index] = heights[index] || 0;
-        if (height > heights[index]) heights[index] = height;
-      });
-
-    });
-
-    tr_copy.each(function (index) {
-      $(this).height(heights[index]);
-    });
-  }
-
+			tx.each(function () {
+				var height = $(this).outerHeight(true);
+				heights[index] = heights[index] || 0;
+				if (height > heights[index]) heights[index] = height;
+			});
+			
+		});
+		
+		tr_copy.each(function (index) {
+			$(this).height(heights[index]);
+		});
+	}
+	
 });
