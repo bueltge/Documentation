d5ed99684277d0dde5865b157edfd08f9fc6a525
Frank Bültge <frank@bueltge.de>
2015-01-09 08:47:56 +0100

    Add serch templates

---
+++ parts/content-search.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Documentation
+ * @since 2015-01-09
+ */
+?>
+	
+		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+			
+			<div class="story">
+			<?php
+			if ( current_theme_supports( 'post-thumbnails' ) )
+				the_post_thumbnail();
+			the_excerpt();
+			wp_link_pages();
+			documentation_post_info();
+			
+			tha_content_after();
+			?>
+			</div>
+			
+		</div>
---
+++ search.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Documentation
+ * @version 2015-01-09
+ */
+
+get_header();
+
+if ( have_posts() ) :
+	
+	tha_content_top(); 
+	?>
+		<h2 class="page-title"><?php printf( __( 'Search Results for: %s', 'documentation' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
+	<?php
+	while ( have_posts() ) :
+		the_post();
+		
+		get_template_part( 'parts/content', 'search' );
+		
+	endwhile;
+	
+	tha_content_bottom();
+	
+else:
+	
+	/**
+	 * Include the template for the loop dosn't find and result
+	 * If you will overwrite this in in a child theme the include a file
+	 * called no-results-single.php and that will be used instead.
+	 */
+	get_template_part( 'parts/no-results', 'home' );
+	
+endif;
+
+get_footer();
